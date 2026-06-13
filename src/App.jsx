f79d34b869325973a49b1b0035aec5120db4fe0b
ckysams-lab <ckysams@bcklas.edu.hk>
2026-06-13 23:11:43 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,26 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// 引入版面與頁面
 import Layout from './components/Layout';
 import PreparationPage from './pages/PreparationPage';
 import StartListPrint from './pages/StartListPrint';
 import UmpireConsole from './pages/UmpireConsole';
 import LiveArena from './pages/LiveArena';
+// 👉 確保引入了 LeaguePage
 import LeaguePage from './pages/LeaguePage'; 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* 所有被 Layout 包覆的頁面 */}
         <Route path="/" element={<Layout />}>
           <Route index element={<PreparationPage />} />
           <Route path="umpire" element={<UmpireConsole />} />
           <Route path="live" element={<LiveArena />} />
+          {/* 👉 這裡註冊了班際積分榜的網址 */}
+          <Route path="league" element={<LeaguePage />} /> 
         </Route>
         
-        {/* 列印頁面不需要側邊欄，所以獨立出來 */}
         <Route path="/print" element={<StartListPrint />} />
       </Routes>
     </BrowserRouter>
